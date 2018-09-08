430b2bedbac324bb081f38f796fd04ef9353e090
jangrabhanu <42331987+jangrabhanu@users.noreply.github.com>
2018-09-08 21:34:40 +0530

    assignment 7

---
+++ assignment 7/vowels.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class JavaProgram
+{
+   public static void main(String args[])
+   {
+       String strOrig, strNew;
+       Scanner scan = new Scanner(System.in);
+       
+       System.out.print("Enter a String : ");
+       strOrig = scan.nextLine();
+       
+       System.out.print("Removing Vowels from The String [" +strOrig+ "]\n");
+       strNew = strOrig.replaceAll("[aeiouAEIOU]", "");
+              System.out.print(strNew);
+   }
+}
